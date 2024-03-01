a46e5350473471cb5961a1b47b72144dbb534af2
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-03-01 19:23:32 +0530

    created custom check box UI button

---
+++ app/src/main/java/com/example/heartdiseasedetetectionapp/View/HomeScreen/Components/CustomCheckBox.kt
@@ -0,0 +1,115 @@
+package com.example.heartdiseasedetetectionapp.View.HomeScreen.Components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.heartdiseasedetetectionapp.R
+
+@Preview(showBackground = true)
+@Composable
+fun CustomCheckBox(){
+    var buttonStateYes by remember{mutableStateOf(false)}
+    var buttonStateNo by remember{mutableStateOf(false)}
+
+    Card(modifier = Modifier
+        .height(100.dp)
+        .width(176.dp)
+        .background(Color.Transparent),
+        shape = RoundedCornerShape(10.dp),
+        colors = CardDefaults.cardColors(Color.White)){
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)
+        ) {
+            Row(horizontalArrangement = Arrangement.SpaceBetween) {
+                Column() {
+                    Card(
+                        modifier = Modifier
+                            .width(73.dp)
+                            .height(68.dp)
+                            .background(Color.Transparent)
+                            .border(
+                                width = 2.dp,
+                                color = if (buttonStateYes == false) Color(0xFF000000) else Color(
+                                    0xFF11009E
+                                ),
+                                shape = RoundedCornerShape(10.dp)
+                            )
+                            .clickable { buttonStateYes = !buttonStateYes },
+                        shape = RoundedCornerShape(10.dp),
+                        colors = CardDefaults.cardColors(if(buttonStateYes==false) Color(0xFFFFFFFF) else Color(0xFF11009E))
+                    ) {
+                        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.check_white),
+                                contentDescription = "yes",
+                                modifier = Modifier.size(60.dp)
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(2.dp))
+                    Text("Yes", fontSize = 7.sp, color = Color.Black, fontWeight = FontWeight.Medium,
+                        textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth(0.47f))
+                }
+
+                Spacer(modifier = Modifier.width(10.dp))
+
+                Column() {
+                    Card(
+                        modifier = Modifier
+                            .width(73.dp)
+                            .height(68.dp)
+                            .background(Color.Transparent)
+                            .border(
+                                width = 2.dp,
+                                color = if (buttonStateNo == false) Color(0xFF000000) else Color(
+                                    0xFF11009E
+                                ),
+                                shape = RoundedCornerShape(10.dp)
+                            )
+                            .clickable { buttonStateNo = !buttonStateNo },
+                        shape = RoundedCornerShape(10.dp),
+                        colors = CardDefaults.cardColors(Color.White)
+                    ) {
+
+                    }
+                    Spacer(modifier = Modifier.height(2.dp))
+                    Text("No", fontSize = 7.sp, color = Color.Black, fontWeight = FontWeight.Medium,
+                        textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
+                }
+
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/heartdiseasedetetectionapp/View/HomeScreen/HomeScreenFinal/HomeScreen.kt
+++ app/src/main/java/com/example/heartdiseasedetetectionapp/View/HomeScreen/HomeScreenFinal/HomeScreen.kt
@@ -58,13 +58,20 @@ fun HomeScreen(){
                 CustomTextField("Enter your cholesterol measurement in mg/dl")
                 Spacer(modifier = Modifier.height(12.dp))
 
+                CustomLabel(heading = "Thalach", 22)
+                CustomTextField("Enter your maximum heart rate achieved")
+                Spacer(modifier = Modifier.height(12.dp))
+
                 CustomLabel(heading = "Chest Pain Type", 22)
                 CustomDropDownMenu(items = listOf("Angina", "Atypical angina", "Non-anginal", "Asymptomatic"))
                 Spacer(modifier = Modifier.height(12.dp))
 
                 CustomLabel(heading = "Exercise-induced angina", 22)
                 CustomDropDownMenu(items = listOf("Yes", "No"))
+                Spacer(modifier = Modifier.height(12.dp))
 
+                CustomLabel(heading = "Thalassemia Value", 22)
+                CustomDropDownMenu(items = listOf("Null", "Fixed defect", "normal blood flow", "reversible defect"))
             }
         }
     }
