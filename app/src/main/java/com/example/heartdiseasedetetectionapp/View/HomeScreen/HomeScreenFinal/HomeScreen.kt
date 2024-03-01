2f5cf87e8da7b0b8450ae6afdc2ef7591281e149
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-03-01 06:49:55 +0530

    lazy column initialised and textfields

--- app/src/main/java/com/example/heartdiseasedetetectionapp/View/HomeScreen/HomeScreenFinal/HomeScreen.kt
+++ app/src/main/java/com/example/heartdiseasedetetectionapp/View/HomeScreen/HomeScreenFinal/HomeScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
@@ -28,25 +29,35 @@ fun HomeScreen(){
     Column(modifier = Modifier
         .fillMaxSize()
         .background(colorResource(id = R.color.primary))
-        .padding(10.dp)){
+        .padding(10.dp)) {
 
         Spacer(modifier = Modifier.height(30.dp))
         CardHeading()
         Spacer(modifier = Modifier.height(15.dp))
 
-        CustomLabel(heading = "Gender", 22)
+        LazyColumn() {
+            item {
+                CustomLabel(heading = "Gender", 22)
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                )
+                {
+                    SexCard("Male", painterResource(id = R.drawable.male))
+                    Spacer(modifier = Modifier.width(4.dp))
+                    SexCard("Female", painterResource(id = R.drawable.female))
+                }
+                Spacer(modifier = Modifier.height(12.dp))
 
-        Row(modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround)
-        {
-            SexCard("Male", painterResource(id = R.drawable.male))
-            Spacer(modifier = Modifier.width(4.dp))
-            SexCard("Female", painterResource(id = R.drawable.female))
+                CustomLabel(heading = "Trestbps", 22)
+                CustomTextField("Enter your resting blood pressure")
+                Spacer(modifier = Modifier.height(12.dp))
+
+                CustomLabel(heading = "Chol", 22)
+                CustomTextField("Enter your cholesterol measurement in mg/dl")
+
+
+            }
         }
-        Spacer(modifier = Modifier.height(12.dp))
-
-        CustomLabel(heading = "Heart Rate", 22)
-        CustomTextField("Enter heart rate")
-
     }
 }
