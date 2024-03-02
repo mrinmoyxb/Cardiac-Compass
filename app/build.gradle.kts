c52582836a0cdb1d1b8b88c1b83fc76345b5669b
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-03-02 21:26:11 +0530

    added navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,9 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
