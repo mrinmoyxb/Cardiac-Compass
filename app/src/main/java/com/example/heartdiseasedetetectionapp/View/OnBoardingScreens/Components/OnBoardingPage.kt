a5ea8c03cbb468bcae52cbf9b5af42e734ad8390
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-03-04 18:40:49 +0530

    fixed the onboarding screen

--- app/src/main/java/com/example/heartdiseasedetetectionapp/View/OnBoardingScreens/Components/OnBoardingPage.kt
+++ app/src/main/java/com/example/heartdiseasedetetectionapp/View/OnBoardingScreens/Components/OnBoardingPage.kt
@@ -2,57 +2,88 @@ package com.example.heartdiseasedetetectionapp.View.OnBoardingScreens.Components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.heartdiseasedetetectionapp.R
 
 @Composable
 fun OnBoardingPage(data: Pages, modifier: Modifier = Modifier){
     Column(
-        modifier = modifier
+        modifier = modifier.fillMaxSize().background(Color.Black),
+        verticalArrangement = Arrangement.spacedBy((-70.dp))
     ){
-        Image(painter = painterResource(id = data.image),
-            contentDescription = "on boarding image",
+        Box(modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ){
+            Image(painter = painterResource(id = data.image),
+                contentScale = ContentScale.Inside,
+                contentDescription = "On boarding image")
+        }
+
+        Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(0.6f)
-                .padding(8.dp),
-            contentScale = ContentScale.Crop,
-            alignment = Alignment.Center
-            )
-        Spacer(modifier = Modifier.height(5.dp))
-        OnBoardingText(data = data)
+                .fillMaxHeight()
+                .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp)),
+            colors = CardDefaults.cardColors(Color.Black)
+        ){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 30.dp, start = 30.dp, end = 30.dp)
+            ){
+                Text(text = data.title, fontSize = 30.sp, textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold, color = Color.White, modifier = Modifier.fillMaxWidth())
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Text(text = data.description, fontSize = 18.sp, textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Medium, color = Color.White, modifier = Modifier.fillMaxWidth())
+            }
+        }
     }
 }
 
+
+
+@Preview(showBackground = true)
 @Composable
-fun OnBoardingText(data:Pages){
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.Black)
-            .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        TextComponent(text = data.title, size = 25, fontWeight = FontWeight.SemiBold, color = Color.White)
-        TextComponent(text = data.description, size = 18, fontWeight = FontWeight.Medium, color = Color.White)
-    }
+fun Display1(){
+    OnBoardingPage(data = pages[1])
 }
 
 @Preview(showBackground = true)
 @Composable
-fun Display(){
+fun Display2(){
+    OnBoardingPage(data = pages[0])
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Display3(){
     OnBoardingPage(data = pages[2])
 }
--- app/src/main/java/com/example/heartdiseasedetetectionapp/View/OnBoardingScreens/Components/PagesData.kt
+++ app/src/main/java/com/example/heartdiseasedetetectionapp/View/OnBoardingScreens/Components/PagesData.kt
@@ -10,7 +10,7 @@ data class Pages(
 )
 
 val pages: List<Pages> = listOf(
-    Pages(title = "Know Your Heart", description = "Take control of your health with our AI-powered heart disease detection tool", R.drawable.heart1),
+    Pages(title = "Know Your Heart", description = "Take control of your health with our AI-powered heart disease detection model", R.drawable.heart1),
     Pages(title = "Quick & Easy", description = "Answer a few simple questions and get personalized insights in seconds", R.drawable.heart2),
     Pages(title = "Empowerment, not Diagnosis", description = "This app is designed to inform, not diagnose. Consult your doctor for any concerns", R.drawable.heart3)
 )
