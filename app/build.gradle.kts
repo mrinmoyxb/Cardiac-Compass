5646876b4123926a8f161bb50750af91708d267a
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-03-05 23:29:15 +0530

    retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,10 @@ dependencies {
     // coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+
 }
