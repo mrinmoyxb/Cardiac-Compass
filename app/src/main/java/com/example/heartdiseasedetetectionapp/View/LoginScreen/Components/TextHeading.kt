33432c6c3f590929bf056a46398b734ab9739ca9
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-03-05 10:30:42 +0530

    login Screen UI updated, added new font

--- app/src/main/java/com/example/heartdiseasedetetectionapp/View/LoginScreen/Components/TextHeading.kt
+++ app/src/main/java/com/example/heartdiseasedetetectionapp/View/LoginScreen/Components/TextHeading.kt
@@ -1,17 +1,23 @@
 package com.example.heartdiseasedetetectionapp.View.LoginScreen.Components
 
+import android.annotation.SuppressLint
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.heartdiseasedetetectionapp.ui.theme.montserrat
 
 @Composable
 fun TextHeading(
     text: String,
     fontSize: Int,
     color: Color,
-    fontWeight: FontWeight
+    fontWeight: FontWeight,
+    style: TextStyle = TextStyle(),
+    modifier: Modifier = Modifier
     ){
-    Text(text, fontSize = fontSize.sp, color = color, fontWeight = fontWeight)
+    Text(text, fontSize = fontSize.sp, color = color, fontWeight = fontWeight, fontFamily = montserrat, style = style, modifier = modifier)
 }
--- app/src/main/java/com/example/heartdiseasedetetectionapp/View/LoginScreen/Screen/LoginScreenFinal.kt
+++ app/src/main/java/com/example/heartdiseasedetetectionapp/View/LoginScreen/Screen/LoginScreenFinal.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -20,9 +21,11 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.heartdiseasedetetectionapp.R
 import com.example.heartdiseasedetetectionapp.View.LoginScreen.Components.CustomTextField
 import com.example.heartdiseasedetetectionapp.View.LoginScreen.Components.TextHeading
@@ -37,17 +40,21 @@ fun LoginScreen(){
             .background(Color.White)
             .padding(10.dp)
     ) {
-        Spacer(modifier = Modifier.height(80.dp))
+        Spacer(modifier = Modifier.height(30.dp))
         TextHeading(text = "Welcome to", fontSize = 32, color = Color.Black, fontWeight = FontWeight.Medium)
-        TextHeading(text = "Cardiac Compass", fontSize = 40, color = colorResource(id = R.color.secondary), fontWeight = FontWeight.Bold)
+        TextHeading(text = "Cardiac Compass", fontSize = 40, color = colorResource(id = R.color.secondary),
+            fontWeight = FontWeight.Bold, modifier = Modifier.offset(y = -12.dp))
 
-        Image(painter = painterResource(id = R.drawable.heartlogin), contentDescription = "")
-            //modifier = Modifier.height(800.dp).width(800.dp))
-        //Spacer(modifier = Modifier.height(100.dp))
-        TextHeading(text = "Name", fontSize = 22, color = Color.Black, fontWeight = FontWeight.Medium)
+        Spacer(modifier = Modifier.height(20.dp))
+        Image(painter = painterResource(id = R.drawable.heartlogin), contentDescription = "",
+            modifier = Modifier.padding(10.dp))
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextHeading(text = "Name", fontSize = 22, color = Color.Black, fontWeight = FontWeight.SemiBold)
         CustomTextField("Enter your name", Icons.Filled.Person)
         Spacer(modifier = Modifier.height(20.dp))
-        TextHeading(text = "Date of Birth", fontSize = 22, color = Color.Black, fontWeight = FontWeight.Medium)
+        TextHeading(text = "Date of Birth", fontSize = 22, color = Color.Black, fontWeight = FontWeight.SemiBold)
         CustomTextField("Enter your dob", Icons.Filled.DateRange)
+
     }
 }
